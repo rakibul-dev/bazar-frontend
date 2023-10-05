08602eb656ab51ba599bd3a97bfc53d9551c669a
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-05 20:49:49 +0600

    admin appbar updated

--- src/components/CommonComponents/AdminAppbar/AdminAppbar.jsx
+++ src/components/CommonComponents/AdminAppbar/AdminAppbar.jsx
@@ -13,14 +13,29 @@ import {
 } from "@mui/material";
 import React from "react";
 import NotificationsSharpIcon from "@mui/icons-material/NotificationsSharp";
+import DeleteIcon from "@mui/icons-material/Delete";
 const AdminAppbar = () => {
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static">
           <Toolbar>
-            <Grid container justifyContent="flex-end">
-              <Grid item md={1}>
+            <Grid
+              container
+              justifyContent={{ md: "flex-end", xs: "space-between" }}
+            >
+              <Grid
+                item
+                justifyContent="flex-start"
+                sx={{ display: { md: "none", sm: "block" } }}
+                md={1}
+                xs={1}
+              >
+                <IconButton aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </Grid>
+              <Grid item md={1} display="flex" justifyContent="flex-end">
                 <Stack direction="row" alignItems="center" spacing={1}>
                   <IconButton aria-label="delete" size="medium">
                     <NotificationsSharpIcon fontSize="inherit" />
