5bd43afdd206121285000cf7964c5cad1522f914
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-05 20:38:26 +0600

    component file updated

--- src/theme/component/component.js
+++ src/theme/component/component.js
@@ -2,12 +2,14 @@ import MuiButton from "./Button/ButtonOverride";
 import MuiAppBar from "./AppBar/AppBarOverride";
 import ToolBar from "./ToolBar/ToolBarOverride";
 import IconButton from "./Button/IconButtonOverride";
-
+// import Paper from "./Paper/PaperOverride";
+import MuiPaper from "./Paper/PaperOverride";
 const components = {
   MuiButton,
   MuiAppBar,
   ToolBar,
   IconButton,
+  MuiPaper,
 };
 
 export default components;
