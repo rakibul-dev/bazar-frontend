d93c75cd4e8d4b343ba8483bd4abd585fffe9830
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-08 18:29:46 +0600

    user login and then role based navigatio added

---
+++ src/pages/AuthPages/CustomerLoginPage.jsx
@@ -0,0 +1,82 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../../Redux/Slices/UserSlice";
+import { useNavigate } from "react-router-dom";
+
+const CustomerLoginPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [userName, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const user = useSelector((state) => state.userSlice.user);
+
+  const handleLogin = async () => {
+    try {
+      dispatch(getUser({ username: userName, password })).then(() => {
+        if (user.role == "admin") {
+          navigate("/");
+        } else {
+          console.log("cant navigate");
+          console.log(user);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <Box
+        display="flex"
+        flexDirection="column"
+        gap={2}
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          gap={2}
+          justifyContent="center"
+          alignItems="center"
+          width="40%"
+        >
+          <Typography variant="h4">Login</Typography>
+          <TextField
+            type="text"
+            label="user name"
+            fullWidth
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            value={userName}
+          />
+          <TextField
+            type="password"
+            label="password"
+            fullWidth
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            value={password}
+          />
+          <Button
+            variant="contained"
+            fullWidth
+            onClick={() => {
+              handleLogin();
+            }}
+          >
+            Login
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default CustomerLoginPage;
