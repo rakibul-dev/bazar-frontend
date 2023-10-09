4eaea210e34914f5224c34836db4b3b3162ae202
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-09 18:25:21 +0600

    product category image upload func added

--- src/pages/AdminPages/Category/AdminCategoryCreatePage.jsx
+++ src/pages/AdminPages/Category/AdminCategoryCreatePage.jsx
@@ -1,5 +1,7 @@
-import React, { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 import {
   Box,
@@ -13,10 +15,44 @@ import {
   FormControlLabel,
 } from "@mui/material";
 const AdminCategoryCreatePage = () => {
+  const [categoryImage, setCategoryImage] = useState(null);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    // console.log(data);
+    const { name, parent } = data;
+    const formData = new FormData();
+    formData.append("category-image", categoryImage);
+    formData.append("name", name);
+    formData.append("parent", parent);
+
+    // data.categoryImage = categoryImage;
+
+    const response = await axios.post(
+      "http://localhost:5000/categories/create",
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        withCredentials: true,
+      }
+    );
+
+    console.log({ response });
+  };
+  console.log(watch("example"));
+
   const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-    console.log({ acceptedFiles });
+    setCategoryImage(acceptedFiles[0]);
+    console.log(categoryImage);
   }, []);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
   });
@@ -40,114 +76,118 @@ const AdminCategoryCreatePage = () => {
   ];
   return (
     <>
+      {/* <img src="https://bazar-ghat.s3.amazonaws.com/1357.jpg" alt="aws s3" /> */}
       <Box display="flex" flexDirection="column" gap={2}>
         <Typography variant="h5" fontWeight="bold">
           Add New Product
         </Typography>
-        <Paper>
-          <Box
-            sx={{
-              padding: "25px",
-            }}
-            gap={2}
-          >
-            <Grid container spacing={2}>
-              <Grid item xs={6} md={6}>
-                <TextField
-                  id="outlined-password-input"
-                  label="Name"
-                  type="text"
-                  autoComplete="current-password"
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={6} md={6}>
-                <TextField
-                  id="outlined-select-currency"
-                  select
-                  label="Select"
-                  //   defaultValue="EUR"
-                  //   helperText="Please select your currency"
-                  fullWidth
-                >
-                  {currencies.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
-
-              <Grid item xs={12} md={12}>
-                <Box
-                  sx={{
-                    height: "250px",
-                    width: "100%",
-                    backgroundColor: "#f6f9fc",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    border: "1px solid red",
-                    borderRadius: "10px",
-                  }}
-                >
-                  <div {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    {isDragActive ? (
-                      <p>Drop the files here ...</p>
-                    ) : (
-                      <Box
-                        display="flex"
-                        flexDirection="column"
-                        justifyContent="center"
-                        alignItems="center"
-                        gap={4}
-                      >
-                        <Typography
-                          variant="h6"
-                          fontWeight="bold"
-                          align="center"
-                        >
-                          Drag or select product images.
-                        </Typography>
-                        <Button
-                          variant="outlined"
-                          size="medium"
-                          color="primary"
-                        >
-                          Upload images
-                        </Button>
-                        <Typography
-                          variant="p"
-                          //   fontWeight="bold"
-                          align="center"
-                        >
-                          Upload 280*280 image
-                        </Typography>
-                      </Box>
-                    )}
-                  </div>
-                </Box>
-              </Grid>
-              <Grid item md={12}>
-                <FormControlLabel
-                  control={<Checkbox />}
-                  label="Label"
-                  onChange={(e) => console.log(e.target.value)}
-                />
-              </Grid>
-              <Grid item md={12}>
-                <Button
-                  variant="contained"
-                  size="medium"
-                  sx={{ marginTop: "15px" }}
-                >
-                  Save category
-                </Button>
-              </Grid>
+        {/* <Paper> */}
+        <Box
+          sx={{
+            padding: "25px",
+          }}
+          gap={2}
+        >
+          <Grid container spacing={2}>
+            <Grid item xs={6} md={6}>
+              <TextField
+                id="outlined-password-input"
+                label="Name"
+                type="text"
+                autoComplete="current-password"
+                fullWidth
+                {...register("name")}
+                name="name"
+              />
             </Grid>
-          </Box>
-        </Paper>
+            <Grid item xs={6} md={6}>
+              <TextField
+                id="outlined-select-currency"
+                select
+                label="Select"
+                defaultValue="EUR"
+                // helperText="Please select your currency"
+                fullWidth
+                {...register("parent")}
+                name="parent"
+              >
+                {currencies.map((option) => (
+                  <MenuItem
+                    key={option.value}
+                    value={option.value}
+                    onClick={() => {
+                      console.log(option);
+                    }}
+                  >
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+
+            <Grid item xs={12} md={12}>
+              <Box
+                sx={{
+                  height: "250px",
+                  width: "100%",
+                  backgroundColor: "#f6f9fc",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  border: "1px solid red",
+                  borderRadius: "10px",
+                }}
+              >
+                <div {...getRootProps()}>
+                  {/* <input {...getInputProps()} /> */}
+                  {isDragActive ? (
+                    <p>Drop the files here ...</p>
+                  ) : (
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      justifyContent="center"
+                      alignItems="center"
+                      gap={4}
+                    >
+                      <Typography variant="h6" fontWeight="bold" align="center">
+                        Drag or select product images.
+                      </Typography>
+                      <Button variant="outlined" size="medium" color="primary">
+                        Upload images
+                      </Button>
+                      <Typography
+                        variant="p"
+                        //   fontWeight="bold"
+                        align="center"
+                      >
+                        Upload 280*280 image
+                      </Typography>
+                    </Box>
+                  )}
+                </div>
+              </Box>
+            </Grid>
+            <Grid item md={12}>
+              <FormControlLabel
+                control={<Checkbox />}
+                label="Label"
+                onChange={(e) => console.log(e.target.value)}
+              />
+            </Grid>
+            <Grid item md={12}>
+              <Button
+                variant="contained"
+                size="medium"
+                sx={{ marginTop: "15px" }}
+                onClick={handleSubmit(onSubmit)}
+              >
+                Save category
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+        {/* </Paper> */}
       </Box>
     </>
   );
