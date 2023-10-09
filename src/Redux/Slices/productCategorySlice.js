8e6256f712a9c8cdd915314490cce870ffa423a1
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-09 22:19:38 +0600

    product category slice added

---
+++ src/Redux/Slices/productCategorySlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "axios";
+
+const initialState = {
+  categories: [],
+};
+
+// First, create the thunk
+export const getCategories = createAsyncThunk(
+  "product-categories/get",
+  async () => {
+    const res = await axios
+      .get("http://localhost:5000/categories")
+      .then((res) => res)
+      .catch((err) => console.log({ err }));
+    return res.data;
+  }
+);
+
+export const createProductCategory = async (categoryInfo) => {
+  const res = await axios
+    .post("http://localhost:5000/categories/create", categoryInfo, {
+      withCredentials: true,
+    })
+    .then((res) => res)
+    .catch((err) => console.log({ err }));
+  return res.data;
+};
+
+export const productCategorySlice = createSlice({
+  name: "product-category",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getCategories.fulfilled, (state, action) => {
+      state.categories = action.payload;
+    });
+  },
+});
+
+export default productCategorySlice.reducer;
