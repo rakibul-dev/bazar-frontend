81451445067f18a9a8c351aae7f2fbb55b19bca5
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-10 20:01:16 +0600

    new reducer added

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./Slices/UserSlice";
 import productCategorySlice from "./Slices/productCategorySlice";
+import brandSlice from "./Slices/brandSlice";
 import logger from "redux-logger";
-const appStage = import.meta.env.VITE_APP_STAGES;
+const appStage = import.meta.env.VITE_APP_STAGE;
 
 const store = configureStore({
   reducer: {
     userSlice,
     productCategorySlice,
+    brandSlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: appStage !== "prod",
