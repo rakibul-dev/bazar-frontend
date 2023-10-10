a58714db38f8894240b683e685a32807e07f711c
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-10 20:05:59 +0600

    theme updated

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,47 +1,8 @@
 // import { createTheme, responsiveFontSizes } from "@mui/material";
 import components from "./component/component";
-// // const breakpoints = createBreakpoints({});
 
-// let theme = createTheme({
-//   breakpoints: {
-//     values: {
-//       xs: 0,
-//       sm: 600,
-//       md: 900,
-//       lg: 1200,
-//       xl: 1536,
-//     },
-//   },
-//   palette: {
-//     primary: {
-//       main: "#c2185b",
-//     },
-//   },
-//   typography: {
-//     // h1: {
-//     //   fontSize: 53,
-//     //   [theme.breakpoints.only("sm")]: {
-//     //     fontSize: 71,
-//     //   },
-//     // },
-//   },
-//   components,
-// });
 
-// theme = responsiveFontSizes(theme);
 
-// // theme = createTheme(theme, {
-// //   typography: {
-// //     h2: {
-// //       fontsize: 53,
-// //       [theme.breakpoints.down("md")]: {
-// //         fontsize: "200px",
-// //       },
-// //     },
-// //   },
-// // });
-
-// export default theme;
 
 import { createTheme } from "@mui/material";
 
