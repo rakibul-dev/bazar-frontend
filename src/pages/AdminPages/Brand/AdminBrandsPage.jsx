2c704dfe5794a58a71b893bd3a49be7f589e0bf1
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-10 20:03:01 +0600

    pagination added

--- src/pages/AdminPages/Brand/AdminBrandsPage.jsx
+++ src/pages/AdminPages/Brand/AdminBrandsPage.jsx
@@ -12,22 +12,52 @@ import {
   Fab,
   Typography,
   Avatar,
+  Pagination,
 } from "@mui/material";
 
-import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useEffect } from "react";
+import { useSearchParams, useParams } from "react-router-dom";
 
-const rows = [
-  {
-    name: "Rakib",
-    phone: "0171828****1",
-    ordersCount: 10,
-    email: "mdrakibul.dev@gmail.com",
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getBrands,
+  deleteBrand,
+  setPageNumber,
+  getFilterdBrands,
+} from "../../../Redux/Slices/brandSlice";
+
 const AdminBrandsPage = () => {
+  const dispatch = useDispatch();
+  const { myParam } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const { brands, pagination } = useSelector((state) => state.brandSlice);
+
+  const pageNumber = searchParams.get("page");
+
+  useEffect(() => {
+    dispatch(getBrands());
+  }, []);
+
+  const handlePagiinationChange = (event, value) => {
+    // dispatch(setPageNumber(value));
+    setSearchParams({ page: value });
+    dispatch(getFilterdBrands(value, "approved"));
+  };
+
+  const deleteBrandHandler = (brandId) => {
+    console.log(brandId);
+    dispatch(deleteBrand(brandId));
+  };
   return (
     <>
-      <Box>
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        gap={2}
+      >
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: "100%" }} aria-label="simple table">
             <TableHead>
@@ -40,46 +70,61 @@ const AdminBrandsPage = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell align="left">{row.name}</TableCell>
-                  <TableCell align="left">
-                    <Typography variant="h6" fontSize="14px" fontWeight="bold">
-                      Yamaha
-                    </Typography>
-                  </TableCell>
-                  <TableCell align="left">
-                    {/* <Avatar src="/images/watch.png" /> */}
-                    <img
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUFxLcXGTWXaindZOgmsRyenoMTwSHcZ7Qf_8eUqqvxjZaGNo-4RhGBozJqS5KfbX75bI&usqp=CAU"
-                      alt=""
-                      height="35px"
-                      width="80px"
-                    />
-                  </TableCell>
+              {brands ? (
+                brands.map((brand) => (
+                  <TableRow
+                    key={brand._id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="left">{brand._id}</TableCell>
+                    <TableCell align="left">
+                      <Typography
+                        variant="h6"
+                        fontSize="14px"
+                        fontWeight="bold"
+                      >
+                        {brand.name}
+                      </Typography>
+                    </TableCell>
+                    <TableCell align="left">
+                      {/* <Avatar src="/images/watch.png" /> */}
+                      <img
+                        src={brand.image}
+                        alt=""
+                        height="35px"
+                        width="80px"
+                      />
+                    </TableCell>
 
-                  <TableCell align="left">
-                    <Switch size="small" />
-                  </TableCell>
-                  <TableCell align="center">
-                    <Fab
-                      size="small"
-                      color="primary"
-                      aria-label="add"
-                      disableRipple
-                      disableFocusRipple
-                    >
-                      <AddIcon />
-                    </Fab>
-                  </TableCell>
-                </TableRow>
-              ))}
+                    <TableCell align="left">
+                      <Switch size="small" />
+                    </TableCell>
+                    <TableCell align="center">
+                      <Fab
+                        size="small"
+                        color="primary"
+                        aria-label="add"
+                        disableRipple
+                        disableFocusRipple
+                        onClick={() => deleteBrandHandler(brand._id)}
+                      >
+                        <DeleteIcon />
+                      </Fab>
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <h2>Loading...</h2>
+              )}
             </TableBody>
           </Table>
         </TableContainer>
+        <Pagination
+          count={pagination.total_pages}
+          //   page={page}
+          onChange={handlePagiinationChange}
+          color="primary"
+        />
       </Box>
     </>
   );
