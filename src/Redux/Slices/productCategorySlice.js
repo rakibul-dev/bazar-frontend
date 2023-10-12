611fb4b79a5922fe5c81ea695c4401252e596479
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-12 23:56:05 +0600

    product category api added

--- src/Redux/Slices/productCategorySlice.js
+++ src/Redux/Slices/productCategorySlice.js
@@ -1,32 +1,53 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-
+const baseUrl = import.meta.env.VITE_BASE_URL;
 import axios from "axios";
 
 const initialState = {
   categories: [],
 };
 
-// First, create the thunk
-export const getCategories = createAsyncThunk(
-  "product-categories/get",
-  async () => {
-    const res = await axios
-      .get("http://localhost:5000/categories")
-      .then((res) => res)
-      .catch((err) => console.log({ err }));
-    return res.data;
-  }
-);
-
 export const createProductCategory = async (categoryInfo) => {
   const res = await axios
-    .post("http://localhost:5000/categories/create", categoryInfo, {
+    .post(`${baseUrl}/categories/create`, categoryInfo, {
       withCredentials: true,
     })
     .then((res) => res)
     .catch((err) => console.log({ err }));
   return res.data;
 };
+
+export const getCategories = createAsyncThunk(
+  "product-categories/get",
+  async () => {
+    const res = await axios
+      .get(`${baseUrl}/categories`)
+      .then((res) => res)
+      .catch((err) => console.log({ err }));
+    return res.data;
+  }
+);
+
+export const updateCategory = createAsyncThunk(
+  "product-categories/update",
+  async () => {
+    const res = await axios
+      .get(`${baseUrl}/categories`)
+      .then((res) => res)
+      .catch((err) => console.log({ err }));
+    return res.data;
+  }
+);
+
+export const deleteCategory = createAsyncThunk(
+  "product-categories/delete",
+  async (id) => {
+    const res = await axios
+      .delete(`${baseUrl}/categories/${id}`, { withCredentials: true })
+      .then((res) => res)
+      .catch((err) => console.log({ err }));
+    return res.data;
+  }
+);
 
 export const productCategorySlice = createSlice({
   name: "product-category",
@@ -35,6 +56,13 @@ export const productCategorySlice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(getCategories.fulfilled, (state, action) => {
       state.categories = action.payload;
+    });
+    builder.addCase(deleteCategory.fulfilled, (state, action) => {
+      const filteredArr = state.categories.filter(
+        (category) => category._id !== action.payload._id
+      );
+      console.log({ filteredArr });
+      state.categories = filteredArr;
     });
   },
 });
