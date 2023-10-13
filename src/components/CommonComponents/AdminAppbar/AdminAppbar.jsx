42a2cc675300a1036d28e8e5768f1622d99330f7
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-14 02:25:20 +0600

    admin logout menu added

--- src/components/CommonComponents/AdminAppbar/AdminAppbar.jsx
+++ src/components/CommonComponents/AdminAppbar/AdminAppbar.jsx
@@ -11,10 +11,32 @@ import {
   Badge,
   Avatar,
 } from "@mui/material";
-import React from "react";
+import { useState } from "react";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import NotificationsSharpIcon from "@mui/icons-material/NotificationsSharp";
 import DeleteIcon from "@mui/icons-material/Delete";
+
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logOutUser } from "../../../Redux/Slices/UserSlice";
 const AdminAppbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(logOutUser()).then((res) => {
+      navigate("/");
+    });
+  };
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -44,7 +66,22 @@ const AdminAppbar = () => {
                     <NotificationsSharpIcon fontSize="inherit" />
                   </IconButton> */}
 
-                  <Avatar src="/images/watch.png" sizes="large" />
+                  <Avatar
+                    src="/images/watch.png"
+                    sizes="large"
+                    onClick={handleClick}
+                  />
+                  <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      "aria-labelledby": "basic-button",
+                    }}
+                  >
+                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                  </Menu>
                 </Stack>
               </Grid>
             </Grid>
