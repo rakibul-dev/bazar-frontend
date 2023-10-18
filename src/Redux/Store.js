e76bdfb3bd1b4c32dae351b8dccede11a1149e91
rakibul-dev <mdrakibul.dev@gmail.com>
2023-10-18 22:47:21 +0600

    cartSlice added to the store

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -2,14 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./Slices/UserSlice";
 import productCategorySlice from "./Slices/productCategorySlice";
 import brandSlice from "./Slices/brandSlice";
+import productSlice from "./Slices/productSlice";
+import cartSlice from "./Slices/cartSlice";
+
 import logger from "redux-logger";
 const appStage = import.meta.env.VITE_APP_STAGE;
 
 const store = configureStore({
   reducer: {
     userSlice,
-    productCategorySlice,
+    cartSlice,
     brandSlice,
+    productSlice,
+    productCategorySlice,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: appStage !== "PROD",
