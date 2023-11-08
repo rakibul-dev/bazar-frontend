e6aeab2371ff7289a88d270e35d73bbd408b78c3
rakibul-dev <mdrakibul.dev@gmail.com>
2023-11-09 03:16:28 +0600

    main file updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,25 @@ import App from "./App.jsx";
 import "./index.css";
 import store from "./Redux/Store.js";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <Provider store={store}>
       <App />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </Provider>
   </>
 );
